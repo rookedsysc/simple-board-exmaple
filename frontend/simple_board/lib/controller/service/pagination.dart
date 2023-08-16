184fe5326951fbdedb09ea1885973ee008629722
rookedsysc <rookedsysc36@gmail.com>
2023-08-16 14:24:42 +0900

    feat : Cursor Pagination Logic 생성

---
+++ frontend/simple_board/lib/controller/service/pagination.dart
@@ -0,0 +1,103 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:simple_board/common/interface/entity_base.dart';
+import 'package:simple_board/common/interface/repository_base.dart';
+import 'package:simple_board/common/model/pagination_model.dart';
+import 'package:simple_board/common/model/pagination_params.dart';
+
+class PaginationProvider<E extends EntityBase,
+        R extends RepositoryBase<E, CursorPagination<E>>>
+    extends StateNotifier<CursorPaginationBase> {
+  final RepositoryBase<E, CursorPagination<E>> _repository;
+  PaginationProvider(
+      {required RepositoryBase<E, CursorPagination<E>> repository})
+      : _repository = repository,
+        super(CursorPaginationLoading()) {
+    paginate();
+  }
+
+  Future<void> paginate({
+    int size = 20,
+    bool fetchMore = false,
+    bool forceRefetch = false,
+  }) async {
+    try {
+      if (state is CursorPagination && !forceRefetch) {
+        final pState = state as CursorPagination<E>;
+
+        if (!_hasMore()) {
+          return;
+        }
+      }
+
+      // 데이터가 로딩 중인 상태
+      if (fetchMore && (_isLoading())) {
+        return;
+      }
+
+      // Pagination Params 생성
+      PaginationParams paginationParams = PaginationParams(page: 0);
+
+      if (fetchMore) {
+        paginationParams = _fetchMore(paginationParams);
+      } else {
+        _fetchFirst(forceRefetch);
+      }
+
+      final resp = await _repository.paginate(paginationParams);
+
+      if (state is CursorPaginationFetchingMore) {
+        final pState = state as CursorPaginationFetchingMore<E>;
+        state = resp.copyWith(
+          data: pState.data + resp.data,
+        );
+      } else {
+        state = resp;
+      }
+    } catch (e) {
+      state = CursorPaginationError(
+          message: "Error while paginating: ${e.toString()}");
+    }
+  }
+
+  bool _hasMore() {
+    if (state is CursorPagination) {
+      final pState = state as CursorPagination<E>;
+      return pState.meta.page != pState.meta.totalPages;
+    }
+    return false;
+  }
+
+  bool _isLoading() {
+    return state is CursorPaginationLoading ||
+        state is CursorPaginationRefetching ||
+        state is CursorPaginationFetchingMore;
+  }
+
+  PaginationParams _fetchMore(PaginationParams paginationParams) {
+    // fetchMore를 실행할 수 있는 상황은 이미 데이터를 불러온 상황이므로 CursorPagination이 들고 있는 상태
+    // IModelWithId를 상속받은 데이터 타입을 들고 있는 상태
+    final pState = state as CursorPagination<E>;
+
+    // 현재 상태를 인자 값을 그대로 유지한 채로
+    // CursorPaginationFetchMore로 변경
+    state = CursorPaginationFetchingMore(data: pState.data, meta: pState.meta);
+    paginationParams = paginationParams.copyWith(
+      // after 값에 현재 데이터(상태)의 마지막 데이터의 id를 넣어줌
+      page: pState.meta.page + 1,
+    );
+    return paginationParams;
+  }
+
+  void _fetchFirst(bool forceRefetch) {
+    // 만약에 데이터가 있는 상황이라면
+    // 기존 데이터를 보존한채로  Fetch(API 요청)를 진행
+    if (state is CursorPagination && !forceRefetch) {
+      final pState = state as CursorPagination<E>;
+      // 데이터는 있는데 새로고침을 하고 있음
+      state =
+          CursorPaginationRefetching<E>(meta: pState.meta, data: pState.data);
+    } else {
+      state = CursorPaginationLoading();
+    }
+  }
+}
