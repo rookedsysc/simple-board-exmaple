aea5ce24d0d4d9e0f06074aa05bac8acdf565764
rookedsysc <rookedsysc36@gmail.com>
2023-08-20 06:13:49 +0900

    feat : Editable Item Interface 추가

---
+++ frontend/simple_board/lib/common/interface/editable_item.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+abstract class EditableItem extends ConsumerWidget {
+  final Widget child;
+  final VoidCallback? onDeletePress;
+  final VoidCallback? onEditPress;
+  final GestureTapCallback? onTap;
+
+  const EditableItem(
+      {required this.child,
+      this.onDeletePress,
+      this.onEditPress,
+      this.onTap,
+      super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return GestureDetector(
+        onTap: onTap, onLongPress: _onLongPress(context, ref), child: child);
+  }
+
+  void Function() _onLongPress(BuildContext context, WidgetRef ref) {
+    return () {
+      ThemeData appTheme = Theme.of(context);
+      double fontSize = 20;
+      showCupertinoModalPopup(
+        context: context,
+        builder: (context) {
+          List<CupertinoActionSheetAction> actions = [];
+
+          ///# Delete Button
+          if (onDeletePress != null) {
+            actions.add(
+              CupertinoActionSheetAction(
+                onPressed: onDeletePress!,
+                child: Text(
+                  'Delete',
+                  style: appTheme.textTheme.bodyMedium
+                      ?.copyWith(fontSize: fontSize, color: Colors.red),
+                ),
+              ),
+            );
+          }
+
+          if (onEditPress != null) {
+            actions.add(
+              CupertinoActionSheetAction(
+                onPressed: onEditPress!,
+                child: Text(
+                  'Delete',
+                  style: appTheme.textTheme.bodyMedium
+                      ?.copyWith(fontSize: fontSize, color: Colors.red),
+                ),
+              ),
+            );
+          }
+
+          return CupertinoActionSheet(
+            actions: <CupertinoActionSheetAction>[...actions],
+          );
+        },
+      );
+    };
+  }
+}
